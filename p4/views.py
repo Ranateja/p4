6c2aa8eada4026927f033bffeffc8f28f8c82699
unknown <ranateja.konda@gmail.com>
2020-07-22 00:34:30 +0530

    commit

--- p4/views.py
+++ p4/views.py
@@ -6,3 +6,10 @@ def home(request):
     return render(request,"sample1.html")
 def second(request):
     return render(request,"directory/sample2.html")
+def third(request):
+    return render(request,"directory/sample3.html",context={'data':'Rana','name':'Ranateja'})
+def fourth(request):
+    fruits=['apple','mango','banana','pineapple','avacado']
+    return render(request,"directory/sample4.html",{'fruits':fruits})
+def fifth(request):
+    return render(request,"directory/sample5.html",{'a':10,'b':20})
